e25da4870df3aae9e4a4f36526207e6dd6df7b0f
eduardocs90 <eduardo1990350@gmail.com>
2023-08-26 13:25:36 -0300

    atualização

---
+++ Listas_lp/src/br/com/poo/lista1/Exercicio1.java
@@ -0,0 +1,39 @@
+package br.com.poo.lista1;
+
+import java.util.Scanner;
+
+//programa do portugol
+public class Exercicio1 {
+	
+	//fução inicio do portugol
+	//função no portugol agora -> metodo no java
+	
+	public static void main(String[] args) {
+		String nome;
+		String sobrenome;
+		Scanner sc = new Scanner (System.in); //biblioteca
+		
+		//funçao escreva do portugol:
+		
+		System.out.println("por favor , informe seu nome: ");
+		nome = sc.nextLine ();// faz leitura de string
+		
+		//utilização do laço for pra limpar a tela
+		for (int i=0; i<20;i++) {
+			System.out.println("\n");
+		}
+		System.out.println("agora informe seu sobre nome: ");
+		sobrenome =sc.next();
+		sc.close(); // fecha o Scanner(biblioteca)
+		
+		
+		System.out.println("Seja bem-vindos " + nome + " "+ sobrenome);
+		}
+		public static void limpa() {
+			for (int i=0; i<20;i++) {
+				System.out.println();			
+		}
+		}
+
+}
+//botão direto do mause encima do arquivo -> run as-> java aplication ( para executar o code)
